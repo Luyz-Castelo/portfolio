538cf5135109c96e13d8e3b0beb0ed9409baccf1
Luyz-Castelo <59575972+Luyz-Castelo@users.noreply.github.com>
2022-12-01 21:27:11 +0000

    creation of header component and usage in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Button from '@mui/material/Button';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Button variant='contained'>Hello world</Button>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,6 @@
+export const Header = () => {
+  // learn how to use MUI components and create header using them
+  return (
+    <></>
+  );
+}
