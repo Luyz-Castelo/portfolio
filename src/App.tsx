30fe11563af1382c7b123650de2fb0b6724b8b29
Luyz-Castelo <59575972+Luyz-Castelo@users.noreply.github.com>
2022-12-01 20:54:20 +0000

    Removal of css files (except reset) and styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import './reset.css'
-import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Button variant='contained'>Hello world</Button>
     </div>
   );
 }
