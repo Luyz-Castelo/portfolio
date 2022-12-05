804373e006f83ad09911db6b56ce1690799999bb
Luyz-Castelo <59575972+Luyz-Castelo@users.noreply.github.com>
2022-12-05 22:47:10 +0000

    Separation of theme to another file

---
+++ src/components/Header/Header.theme.tsx
@@ -0,0 +1,46 @@
+import { createTheme, responsiveFontSizes } from "@mui/material";
+
+let theme = createTheme({
+  typography: {
+    h6: {
+      color: '#5e5e5e',
+      fontWeight: 'bold',
+      ":hover": {
+        color: '#4b6cc1',
+      },
+    },
+  },
+  components: {
+    MuiLink: { styleOverrides: { root: { textDecoration: 'none' } } },
+    MuiContainer: {
+      variants: [
+        {
+          props: { id: 'blue-box' },
+          style: {
+            backgroundColor: '#4b6cc1',
+            height: '1vh',
+            borderRadius: '2vh'
+          }
+        },
+        {
+          props: { id: 'links' },
+          style: {
+            display: 'flex',
+            paddingLeft: '0 !important',
+            position: 'absolute',
+            gap: '3vw',
+            bottom: '0',
+          },
+        },
+      ],
+    },
+    MuiAvatar: {
+      styleOverrides: { root: { width: '100%', height: '100%' } }
+    },
+  }
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
+
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,53 +1,62 @@
+import theme from './Header.theme';
 import Luyz from '../../public/Images/luyz.jpg' 
 import Grid from '@mui/material/Unstable_Grid2';
-import { Container, Link, Typography, Avatar, Box } from "@mui/material";
+import { 
+  Container,
+  Link,
+  Typography,
+  Avatar,
+  ThemeProvider,
+} from "@mui/material";
 
 export const Header = () => {
   return (
-    <header>
-      <Container maxWidth="md">
-        <Grid container mt={2} position='relative'>
-          <Grid xs={2} md={2}>
-            <Link href='/' color='inherit'>
-              <Avatar alt='Foto do Luyz' src={Luyz} sx={{ width: '100%', height: '100%'}} />
-            </Link>
-          </Grid>
-          <Grid xs={1} md={1} />
-          <Grid xs={9} md={9}>
-            <Typography variant='h4'>
-              Luyz Castelo Branco
-            </Typography>
-            <Grid container>
-              <Grid xs={1} md={1} mt={1}>
-                <Box sx={{ backgroundColor: '#4b6cc1', height: '1vh', borderRadius: '2vh' }} />
-              </Grid>
+    <ThemeProvider theme={theme}>
+      <header>
+        <Container maxWidth="md">
+          <Grid container mt={2} position='relative'>
+            <Grid xs={2} md={2}>
+              <Link href='/'>
+                <Avatar src={Luyz} alt='Foto do Luyz' />
+              </Link>
             </Grid>
+            <Grid xs={1} md={1} />
+            <Grid xs={9} md={9}>
+              <Typography variant='h4'>
+                Luyz Castelo Branco
+              </Typography>
+              <Grid container>
+                <Grid xs={1} md={1} mt={1}>
+                  <Container id='blue-box' />
+                </Grid>
+              </Grid>
 
-            <Container sx={{ display: 'flex', paddingLeft: '0 !important', position: 'absolute', gap: '3vw', bottom: '0' }} >
-              <Typography variant='h6' color='#5e5e5e'>
-                <Link href='https://github.com/Luyz-Castelo' fontWeight='bold' color='inherit' underline='none' sx={{'&:hover': {color: '#4b6cc1'}}}>
-                  Work
+              <Container id='links'>
+                <Link href='https://github.com/Luyz-Castelo'>
+                  <Typography variant='h6'>
+                    Work
+                  </Typography>
                 </Link>
-              </Typography>
-              <Typography variant='h6' color='#5e5e5e'>
-                <Link href='https://github.com/Luyz-Castelo' fontWeight='bold' color='inherit' underline='none' sx={{'&:hover': {color: '#4b6cc1'}}}>
-                  Projects
+                <Link href='https://github.com/Luyz-Castelo'>
+                  <Typography variant='h6'>
+                    Projects
+                  </Typography>
                 </Link>
-              </Typography>
-              <Typography variant='h6' color='#5e5e5e'>
-                <Link href='https://github.com/Luyz-Castelo' fontWeight='bold' color='inherit' underline='none' sx={{'&:hover': {color: '#4b6cc1'}}}>
-                  Articles
+                <Link href='https://github.com/Luyz-Castelo'>
+                  <Typography variant='h6'>
+                    Articles
+                  </Typography>
                 </Link>
-              </Typography>
-              <Typography variant='h6' color='#5e5e5e'>
-                <Link href='https://github.com/Luyz-Castelo' fontWeight='bold' color='inherit' underline='none' sx={{'&:hover': {color: '#4b6cc1'}}}>
-                  Contact
+                <Link href='https://github.com/Luyz-Castelo'>
+                  <Typography variant='h6'>
+                    Contact
+                  </Typography>
                 </Link>
-              </Typography>
-            </Container>
+              </Container>
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
-    </header>
+        </Container>
+      </header>
+    </ThemeProvider>
   );
 }
