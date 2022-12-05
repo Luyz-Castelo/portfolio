f6d68aa98004950425b47fdb4241bad4a4fe90d5
Luyz-Castelo <59575972+Luyz-Castelo@users.noreply.github.com>
2022-12-05 20:13:32 +0000

    Correction of header links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,13 @@ import Grid from '@mui/material/Unstable_Grid2';
 import { Container, Link, Typography, Avatar, Box } from "@mui/material";
 
 export const Header = () => {
-  // learn how to use MUI components and create header using them
   return (
     <header>
       <Container maxWidth="md">
-        <Grid container mt={2}>
+        <Grid container mt={2} position='relative'>
           <Grid xs={2} md={2}>
-            <Link href='https://github.com/Luyz-Castelo' color='inherit'>
-              <Avatar alt='Foto do Luyz' src={Luyz} sx={{width: '100%', height: '100%'}} />
+            <Link href='/' color='inherit'>
+              <Avatar alt='Foto do Luyz' src={Luyz} sx={{ width: '100%', height: '100%'}} />
             </Link>
           </Grid>
           <Grid xs={1} md={1} />
@@ -23,36 +22,29 @@ export const Header = () => {
                 <Box sx={{ backgroundColor: '#4b6cc1', height: '1vh', borderRadius: '2vh' }} />
               </Grid>
             </Grid>
-            <Grid container mt={4} color='#5e5e5e'>
-              <Grid xs={1} md={1} mr={5}>
-                <Typography variant='h6'>
-                  <Link href='https://github.com/Luyz-Castelo' fontWeight='bold' color='inherit' underline='none' sx={{'&:hover': {color: '#4b6cc1'}}}>
-                    Work
-                  </Link>
-                </Typography>
-              </Grid>
-              <Grid xs={1} md={1} mr={5}>
-                <Typography variant='h6'>
-                  <Link href='https://github.com/Luyz-Castelo' fontWeight='bold' color='inherit' underline='none' sx={{'&:hover': {color: '#4b6cc1'}}}>
-                    Projects
-                  </Link>
-                </Typography>
-              </Grid>
-              <Grid xs={1} md={1} mr={5}>
-                <Typography variant='h6'>
-                  <Link href='https://github.com/Luyz-Castelo' fontWeight='bold' color='inherit' underline='none' sx={{'&:hover': {color: '#4b6cc1'}}}>
-                    Articles
-                  </Link>
-                </Typography>
-              </Grid>
-              <Grid xs={1} md={1} mr={5}>
-                <Typography variant='h6'>
-                  <Link href='https://github.com/Luyz-Castelo' fontWeight='bold' color='inherit' underline='none' sx={{'&:hover': {color: '#4b6cc1'}}}>
-                    Contact
-                  </Link>
-                </Typography>
-              </Grid>
-            </Grid>
+
+            <Container sx={{ display: 'flex', paddingLeft: '0 !important', position: 'absolute', gap: '3vw', bottom: '0' }} >
+              <Typography variant='h6' color='#5e5e5e'>
+                <Link href='https://github.com/Luyz-Castelo' fontWeight='bold' color='inherit' underline='none' sx={{'&:hover': {color: '#4b6cc1'}}}>
+                  Work
+                </Link>
+              </Typography>
+              <Typography variant='h6' color='#5e5e5e'>
+                <Link href='https://github.com/Luyz-Castelo' fontWeight='bold' color='inherit' underline='none' sx={{'&:hover': {color: '#4b6cc1'}}}>
+                  Projects
+                </Link>
+              </Typography>
+              <Typography variant='h6' color='#5e5e5e'>
+                <Link href='https://github.com/Luyz-Castelo' fontWeight='bold' color='inherit' underline='none' sx={{'&:hover': {color: '#4b6cc1'}}}>
+                  Articles
+                </Link>
+              </Typography>
+              <Typography variant='h6' color='#5e5e5e'>
+                <Link href='https://github.com/Luyz-Castelo' fontWeight='bold' color='inherit' underline='none' sx={{'&:hover': {color: '#4b6cc1'}}}>
+                  Contact
+                </Link>
+              </Typography>
+            </Container>
           </Grid>
         </Grid>
       </Container>
